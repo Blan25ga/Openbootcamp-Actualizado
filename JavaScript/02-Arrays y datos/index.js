71d4038501bfd3c963fe69e4434d858734840016
Blan25ga <gabriel.bl@hotmail.com.ar>
2022-06-14 14:24:44 -0300

    02-Sintaxix,Arrays

---
+++ JavaScript/02-Arrays y datos/index.js
@@ -0,0 +1,13 @@
+const datos = [
+  "Gabriel",
+  34,
+  true,
+  new Date(1986, 11, 17),
+
+  {
+    titulo: "Poker Texas Hold'em El poder de la estrategia",
+    autor: "Daniel Negreanu",
+    fecha: new Date(2015, 5, 1),
+    url: "https://www.amazon.com/-/es/Daniel-Negreanu/dp/1724135864/",
+  },
+];
